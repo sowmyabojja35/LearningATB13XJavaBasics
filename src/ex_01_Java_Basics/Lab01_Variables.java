247fa5c804e6dd6cad27de29929e844dd76668c8
MSI\reddy <mreddy@ashleystewart.com>
2025-06-11 21:04:27 -0500

    second commit

---
+++ src/ex_01_Java_Basics/Lab01_Variables.java
@@ -0,0 +1,16 @@
+package ex_01_Java_Basics;
+
+public class Lab01_Variables {
+    public static void main(String[] args) {
+        byte table = 9;
+        // Data_Type Variable_Name = Variable_Value
+        // Byte- Date Type- 8bits
+        //b- Variable name-Identifier
+        //= Assignment operator
+        //10- variable value- Literal- Integer value
+        System.out.printf("%d * 1 = %d\n",table, table);
+        System.out.printf("%d * 2 = %d\n",table, table*2);
+
+    }
+
+}
